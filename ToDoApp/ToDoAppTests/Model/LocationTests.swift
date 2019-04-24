97e0719bf2d03c03e2d8ce7daa7cda9067828dac
Anastasiia <anastasia.halk27@gmail.com>
2019-04-24 17:14:36 +0300

    task location property2

--- ToDoApp/ToDoAppTests/Model/LocationTests.swift
+++ ToDoApp/ToDoAppTests/Model/LocationTests.swift
@@ -19,12 +19,21 @@ class LocationTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
+    func testInitSetsName() {
+        let location = Location(name: "Foo")
+        XCTAssertEqual(location.name, "Foo")
+    }
+    
     func testInitSetsCoordinates() {
         let coordinate = CLLocationCoordinate2D(latitude: 1,
                                                 longitude: 2)
         
         let location = Location(name: "Foo",
                                 coordinate: coordinate)
+        
+        XCTAssertEqual(location.coordinate?.latitude, coordinate.latitude)
+        XCTAssertEqual(location.coordinate?.longitude, coordinate.longitude)
+
     }
 
 }
