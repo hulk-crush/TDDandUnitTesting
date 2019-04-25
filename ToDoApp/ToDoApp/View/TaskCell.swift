b7c68eb25e1795afe7472f856a004575a4918f5a
Anastasiia <anastasia.halk27@gmail.com>
2019-04-25 18:49:30 +0300

    Data provider

---
+++ ToDoApp/ToDoApp/View/TaskCell.swift
@@ -0,0 +1,13 @@
+//
+//  TaskCell.swift
+//  ToDoApp
+//
+//  Created by Admin on 25/04/2019.
+//  Copyright © 2019 Admins. All rights reserved.
+//
+
+import UIKit
+
+class TaskCell: UITableViewCell {
+
+}
---
+++ ToDoApp/ToDoAppTests/Controller/DataProviderTests.swift
@@ -0,0 +1,71 @@
+//
+//  DataProviderTests.swift
+//  ToDoAppTests
+//
+//  Created by Admin on 25/04/2019.
+//  Copyright © 2019 Admins. All rights reserved.
+//
+
+import XCTest
+@testable import ToDoApp
+class DataProviderTests: XCTestCase {
+
+    var sut: DataProvider!
+    var tableView: UITableView!
+    
+    override func setUp() {
+        sut = DataProvider()
+        sut.taskManager = TaskManager()
+
+        tableView = UITableView()
+        tableView.dataSource = sut
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testNumberOfSectionsIsTwo() {
+        let numberOfSections = tableView.numberOfSections
+        
+        XCTAssertEqual(numberOfSections, 2)
+    }
+
+    func testNumberOfRowsInSectionsZeroIsTasksCount() {
+        sut.taskManager?.add(task: Task(title: "Foo"))
+        
+        XCTAssertEqual(tableView.numberOfRows(inSection: 0), 1)
+        
+        sut.taskManager?.add(task: Task(title: "Bar"))
+
+        tableView.reloadData()
+        
+        XCTAssertEqual(tableView.numberOfRows(inSection: 0), 2)
+    }
+
+    func testNumberOfRowsInSectionsOneIsDoneTasksCount() {
+        sut.taskManager?.add(task: Task(title: "Foo"))
+        sut.taskManager?.checkTask(at: 0)
+        
+        XCTAssertEqual(tableView.numberOfRows(inSection: 1), 1)
+        
+        sut.taskManager?.add(task: Task(title: "Bar"))
+        sut.taskManager?.checkTask(at: 0)
+
+        tableView.reloadData()
+        
+        XCTAssertEqual(tableView.numberOfRows(inSection: 1), 2)
+    }
+    
+    func testCellForRowAtIndexPathReturnsTaskCell() {
+        sut.taskManager?.add(task: Task(title: "Foo"))
+        tableView.reloadData()
+        
+        let cell = tableView.cellForRow(at: IndexPath(row: 0, section: 0))
+        
+        XCTAssertTrue(cell is TaskCell)
+        
+        
+    }
+    
+}
