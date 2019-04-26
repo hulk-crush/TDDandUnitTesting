6a43318be5086943678e52859f5d9c765c1a5d1a
Anastasiia <anastasia.halk27@gmail.com>
2019-04-26 16:27:41 +0300

    Mock Table View

--- ToDoApp/ToDoApp/Controller/DataProvider.swift
+++ ToDoApp/ToDoApp/Controller/DataProvider.swift
@@ -39,7 +39,16 @@ extension DataProvider: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return TaskCell()
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: TaskCell.self), for: indexPath) as! TaskCell
+        
+        
+        
+        
+        
+        
+        
+        return cell
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
