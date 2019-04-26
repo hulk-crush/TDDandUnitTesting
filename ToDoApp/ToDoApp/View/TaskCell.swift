d3eb26b4bd01ee9cd9c2bc14eee1452b6ffae0d2
Anastasiia <anastasia.halk27@gmail.com>
2019-04-26 16:54:48 +0300

    Mock task cell

--- ToDoApp/ToDoApp/View/TaskCell.swift
+++ ToDoApp/ToDoApp/View/TaskCell.swift
@@ -9,5 +9,7 @@
 import UIKit
 
 class TaskCell: UITableViewCell {
-
+    func configure(withTask task: Task) {
+        
+    }
 }
--- ToDoApp/ToDoAppTests/Controller/DataProviderTests.swift
+++ ToDoApp/ToDoAppTests/Controller/DataProviderTests.swift
@@ -87,6 +87,19 @@ class DataProviderTests: XCTestCase {
         XCTAssertTrue(mockTableView.cellIsDequeued)
     }
     
+    func testCellForRowInSectionZeroCallsConfigure() {
+        tableView.register(MockTaskCell.self, forCellReuseIdentifier: String(describing: TaskCell.self))
+        
+        let task = Task(title: "Foo")
+        sut.taskManager?.add(task: task)
+        tableView.reloadData()
+        
+       let cell = tableView.cellForRow(at: IndexPath(row: 0, section: 0)) as! MockTaskCell
+        
+        XCTAssertEqual(cell.task, task)
+        
+    }
+    
 }
 
 extension DataProviderTests {
@@ -99,4 +112,13 @@ extension DataProviderTests {
             return super.dequeueReusableCell(withIdentifier: identifier, for: indexPath)
         }
     }
+    
+    class MockTaskCell: TaskCell {
+        var task: Task?
+        
+        func configure(withTask task: Task) {
+            self.task = task
+        }
+    }
+    
 }
